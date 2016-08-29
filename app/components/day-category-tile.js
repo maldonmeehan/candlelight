362dda53e4f5125d9405ddb240eeda45ef551809
maldon <maldonmeehan@gmail.com>
2016-08-29 13:52:28 -0700

    add computed property for biscuits and side orders

--- app/components/day-category-tile.js
+++ app/components/day-category-tile.js
@@ -3,5 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   pancakes: Ember.computed.filterBy('items', 'category', 'pancakes and such'),
 
-  classics: Ember.computed.filterBy('items', 'category', 'classic combos')
+  classics: Ember.computed.filterBy('items', 'category', 'classic combos'),
+
+  biscuits: Ember.computed.filterBy('items', 'category', 'biscuits and side orders')
   });
