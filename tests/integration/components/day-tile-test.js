e4fb4cd0e720b6cc11dc543c56e5ff615a70bc41
maldon <maldonmeehan@gmail.com>
2016-08-29 11:03:28 -0700

    add component for day menu

--- tests/integration/components/day-tile-test.js
+++ tests/integration/components/day-tile-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('day-items', 'Integration | Component | day items', {
+moduleForComponent('day-tile', 'Integration | Component | day tile', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{day-items}}`);
+  this.render(hbs`{{day-tile}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#day-items}}
+    {{#day-tile}}
       template block text
-    {{/day-items}}
+    {{/day-tile}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
