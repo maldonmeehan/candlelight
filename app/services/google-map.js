d989939a8fbbd40b6796c488f035211e5a47f2a5
Meaghan Jones and Olivia Hinton <olivia@>
2016-08-30 14:40:49 -0700

    add icon to google map and link in info window

--- app/services/google-map.js
+++ app/services/google-map.js
@@ -7,5 +7,11 @@ export default Ember.Service.extend({
     },
     center(latitude, longitude) {
       return new this.googleMaps.LatLng(latitude, longitude);
+    },
+    createMarker(markerOptions) {
+      return new this.googleMaps.Marker(markerOptions);
+    },
+    createInfoWindow(content) {
+      return new this.googleMaps.InfoWindow(content);
     }
   });
