fd860f5cdc2b9eed54114208f885d2a1b47be3fe
Meaghan Jones and Olivia Hinton <meaghan@>
2016-08-30 16:17:01 -0700

    add card box to event page

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('location');
-  },
+  model: function(params) {
+    var url = 'https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyBljUWF_JCV0ZVKINXl_ddxVjKE5RbRpRY&placeid=ChIJC9rX5hqhlVQRhcbrOoH0-7A';
+    return Ember.$.getJSON(url).then(function(responseJSON){
+      return responseJSON.results;
+    });
+  }
 });
