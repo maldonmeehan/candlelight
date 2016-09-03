a71a0496850da17ef2ab676e80fe8363e1625adf
maldon <maldonmeehan@gmail.com>
2016-09-03 16:51:55 -0700

    update database and day menu

--- app/components/day-category-tile.js
+++ app/components/day-category-tile.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   pancakes: Ember.computed.filterBy('items', 'category', 'pancakes and such'),
 
+  breakfastSpecials: Ember.computed.filterBy('items', 'category', 'breakfast specials'),
+
   classics: Ember.computed.filterBy('items', 'category', 'classic combos'),
 
   biscuits: Ember.computed.filterBy('items', 'category', 'biscuits and side orders'),
